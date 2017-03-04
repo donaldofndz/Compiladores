0596b9aaafd033e19e702762ab1cfda5b59d064c
Donaldo <donaldo.fndz@gmail.com>
2017-03-04 15:43:05 -0600

    ejemplos Lex

--- Compilador/lexico/dos.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-	var numero = 7;
-	printf("%d\n",numero); //ERROR LEXICO DEBIDO A QUE EL COMPILADOR NO RECONOCER LA INTRUCCION VAR
-	return 0;
-}
